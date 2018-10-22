e37e555126d5659c41b474aef00a807837410097
erickkf600 <erickkf600@gmail.com>
2018-10-22 13:06:49 -0300

    Bucador de CEP

---
+++ src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController, IonicPage } from 'ionic-angular';
+import { EnderecoService } from '../../service/endereco.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  formGroup: FormGroup;
+
+  constructor(public navCtrl: NavController,
+              public service: EnderecoService,
+              public formBuilder: FormBuilder) {
+   
+    this.formGroup = this.formBuilder.group({
+      //Campos do formulario
+      cep : ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]]
+    });            
+  }
+
+  buscaCEP() {
+
+    this.service.getEndereco(this.formGroup.value['cep'])
+      .subscribe(response => {
+        console.log(response);
+      });
+
+  }
+}
+
+
